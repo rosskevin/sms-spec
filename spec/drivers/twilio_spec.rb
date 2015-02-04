fbb46d5a0426844f113d9e6841c41ee998a573fb
Kevin Ross <kevin.ross@alienfast.com>
2015-02-04 17:24:22 -0600

    updated twilio_spec with before reuse of before block - no semantic changes yet

--- spec/drivers/twilio_spec.rb
+++ spec/drivers/twilio_spec.rb
@@ -1,63 +1,58 @@
 require 'spec_helper'
 
-describe SmsSpec do
+describe 'twilio-ruby' do
   include SmsSpec::Helpers
 
   before :each do
     SmsSpec::Data.clear_messages
   end
 
-  describe "the twilio-ruby driver" do
-    it "is assignable" do
-      SmsSpec.driver = :"twilio-ruby"
+  it 'is assignable' do
+    SmsSpec.driver = :'twilio-ruby'
+  end
+
+
+  context 'instantiated with args' do
+
+    before do
+      @account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+      @auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+      @client = Twilio::REST::Client.new @account_sid, @auth_token
     end
 
-    it "intercepts calls to twilio's sms client" do
-      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-
-      @client = Twilio::REST::Client.new account_sid, auth_token
+    it 'intercepts calls to twilio sms client' do
       @client.account.messages.create(
-        :from => '+14159341234',
-        :to => '+16105557069',
-        :body => 'Hey there!'
+          :from => '+14159341234',
+          :to => '+16105557069',
+          :body => 'Hey there!'
       )
 
-      open_last_text_message_for("+16105557069")
+      open_last_text_message_for('+16105557069')
       expect(current_text_message).to_not be_nil
     end
 
-    it "allows for sid method calls on the account object" do
-      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-
-      @client = Twilio::REST::Client.new account_sid, auth_token
+    it 'allows for sid method calls on the account object' do
       @client.account.messages.create(
-        :from => '+14159341234',
-        :to => '+16105557069',
-        :body => 'Hey there!'
+          :from => '+14159341234',
+          :to => '+16105557069',
+          :body => 'Hey there!'
       )
 
-      open_last_text_message_for("+16105557069")
+      open_last_text_message_for('+16105557069')
       expect(current_text_message).to_not be_nil
       expect(@client.account).to respond_to(:sid)
-      expect(@client.account.sid).to be(account_sid)
+      expect(@client.account.sid).to be(@account_sid)
     end
 
-    it "records the from number for a message" do
-      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-
-      @client = Twilio::REST::Client.new account_sid, auth_token
+    it 'records the from number for a message' do
       @client.account.messages.create(
-        :from => '+14159341234',
-        :to => '+16105557069',
-        :body => 'Hey there!'
+          :from => '+14159341234',
+          :to => '+16105557069',
+          :body => 'Hey there!'
       )
 
-      open_last_text_message_for("+16105557069")
+      open_last_text_message_for('+16105557069')
       expect(current_text_message.from).to eq('+14159341234')
     end
-
   end
 end
