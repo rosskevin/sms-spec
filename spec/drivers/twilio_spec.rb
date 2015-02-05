8463069cefd6f4285bb1d7b186c6ee5911063e1c
Kevin Ross <kevin.ross@alienfast.com>
2015-02-04 18:07:46 -0600

    1) Allowed for flexible initialization. 2) Solved mocking problem when the twilio README recommends using the method_missing approach to account (it forwards calls to the account object)

--- spec/drivers/twilio_spec.rb
+++ spec/drivers/twilio_spec.rb
@@ -2,17 +2,72 @@ require 'spec_helper'
 
 describe 'twilio-ruby' do
   include SmsSpec::Helpers
+  SmsSpec.driver = :'twilio-ruby'
 
   before :each do
     SmsSpec::Data.clear_messages
   end
 
-  it 'is assignable' do
-    SmsSpec.driver = :'twilio-ruby'
+  context 'instantiation options' do
+
+    before :each do
+      @account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+      @auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+      @client = nil
+    end
+
+    it 'with two args' do
+      @client = Twilio::REST::Client.new @account_sid, @auth_token
+      expect(@client).to_not be_nil
+    end
+
+    it 'with config' do
+      Twilio.configure do |config|
+        config.account_sid = @account_sid
+        config.auth_token = @auth_token
+      end
+      @client = Twilio::REST::Client.new
+      expect(@client).to_not be_nil
+    end
   end
 
 
-  context 'instantiated with args' do
+  context 'using method_missing approach' do
+
+    before do
+      @account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+      @auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+      Twilio.configure do |config|
+        config.account_sid = @account_sid
+        config.auth_token = @auth_token
+      end
+      @client = Twilio::REST::Client.new
+    end
+
+    it 'intercepts calls to twilio sms client' do
+      @client.messages.create(
+          :from => '+14159341234',
+          :to => '+16105557069',
+          :body => 'Hey there!'
+      )
+
+      open_last_text_message_for('+16105557069')
+      expect(current_text_message).to_not be_nil
+    end
+
+    it 'records the from number for a message' do
+      @client.account.messages.create(
+          :from => '+14159341234',
+          :to => '+16105557069',
+          :body => 'Hey there!'
+      )
+
+      open_last_text_message_for('+16105557069')
+      expect(current_text_message.from).to eq('+14159341234')
+    end
+  end
+
+  context 'using .account.* methods' do
 
     before do
       @account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
@@ -29,19 +84,6 @@ describe 'twilio-ruby' do
 
       open_last_text_message_for('+16105557069')
       expect(current_text_message).to_not be_nil
-    end
-
-    it 'allows for sid method calls on the account object' do
-      @client.account.messages.create(
-          :from => '+14159341234',
-          :to => '+16105557069',
-          :body => 'Hey there!'
-      )
-
-      open_last_text_message_for('+16105557069')
-      expect(current_text_message).to_not be_nil
-      expect(@client.account).to respond_to(:sid)
-      expect(@client.account.sid).to be(@account_sid)
     end
 
     it 'records the from number for a message' do
