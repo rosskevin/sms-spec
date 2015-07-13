786238c1924c055d16a4963abb329c9b985ce104
Moncef Belyamani <moncef.belyamani@gsa.gov>
2015-07-13 09:17:48 -0400

    Don't format phone numbers.
    Numbers should not be altered in any way. Otherwise, it would defeat the purpose of this gem. For example, if I want to test that an invalid number raises an error, and if this gem alters that number so that it becomes valid, I will never be able to test that scenario.

--- lib/sms_spec.rb
+++ lib/sms_spec.rb
@@ -1,12 +1,10 @@
 require 'rspec'
-require "sms_spec/version"
-require "sms_spec/helpers"
-require "sms_spec/data"
-require "sms_spec/message"
-require "sms_spec/util"
-require "sms_spec/mobile_device"
-require "sms_spec/matchers"
-#require "sms_spec/drivers/twilio-ruby"
+require 'sms_spec/version'
+require 'sms_spec/helpers'
+require 'sms_spec/data'
+require 'sms_spec/message'
+require 'sms_spec/mobile_device'
+require 'sms_spec/matchers'
 
 module SmsSpec
   def self.driver=(driver_sym)
--- lib/sms_spec/message.rb
+++ lib/sms_spec/message.rb
@@ -4,10 +4,8 @@ module SmsSpec
     attr_accessor :body
     attr_accessor :from
 
-    include SmsSpec::Util
-
-    def initialize(opts={})
-      @number = sanitize opts[:number]
+    def initialize(opts = {})
+      @number = opts[:number]
       @body = opts[:body]
       @from = opts[:from]
     end
